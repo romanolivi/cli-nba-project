f721a64be7dd4ca1856871c591420f64c9566e6d
romanolivi <romano_livi@outlook.com>
2020-12-28 13:47:50 -0500

    deleted files

--- CLI-NBA-PROJECT/lib/CLI/NBA/PROJECT/version.rb
+++
@@ -1,7 +0,0 @@
-module CLI
-  module NBA
-    module PROJECT
-      VERSION = "0.1.0"
-    end
-  end
-end
--- CLI-NBA-PROJECT/spec/CLI/NBA/PROJECT_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe CLI::NBA::PROJECT do
-  it "has a version number" do
-    expect(CLI::NBA::PROJECT::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
