da15cbe0a532cd32c85b380bdcf27812b34065e7
romanolivi <romano_livi@outlook.com>
2020-12-28 13:58:12 -0500

    updated gemspec file

---
+++ nba_cli/config/environment.rb
@@ -0,0 +1,9 @@
+require "pry"
+require "httparty"
+require "nokogiri"
+require "open-uri"
+
+# require "nbaCLI/version"
+# require "nbaCLI/cli"
+# require "nbaCLI/scraper"
+# require "nbaCLI/roster_info"
